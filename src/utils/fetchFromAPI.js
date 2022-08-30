4116af7e75e31cc79d2a1331ca96882704366af1
Abhinav Anand <arnavanand1500@gmail.com>
2022-08-31 02:09:03 +0530

    Yt clone

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "4416aab032msh3a8524980639e72p17859bjsn4d380d75bf2f",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
